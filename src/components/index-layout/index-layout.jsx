0e63ab64ed761de20d354999fb2e9fdb528f018d
Alan Ibatullin <mr.juneiak@gmail.com>
2022-05-23 13:58:02 +0500

    add halmet

--- src/components/index-layout/index-layout.jsx
+++ src/components/index-layout/index-layout.jsx
@@ -7,6 +7,7 @@ import BurgerMenuButton from '../burger-menu-button/burger-menu-button';
 import NavMenu from '../nav-menu/nav-menu'
 import useDelayUnmountState from '../hooks/use-delay-unmount-state';
 import PopupLayout from '../popup-layout/popup-layout';
+import Helmet from 'react-helmet';
 
 const IndexLayout = ({ children }) => {
   const [isNavMenuOpen, setIsNavMenuOpen] = React.useState(false);
@@ -15,23 +16,33 @@ const IndexLayout = ({ children }) => {
   const animStyle = isNavMenuOpen ? {animation: 'openAniamtion 0.5s linear'} : {animation: 'closeAniamtion 0.5s linear forwards'}
 
   return (
-    <div className={styles.layout}>
+    <>
+      <Helmet htmlAttributes={{lang: 'ru'}}>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Кровати диваны и проч" />
+        <meta name="keywords" content="Диваны, кровати, качественный, специально для вас, уфа, быстро" />
+        <meta name="author" content="CookDog" />
+        <title>Melatika</title>
+      </Helmet>
+      <div className={styles.layout}>
 
-      {children}
+        {children}
 
-      <div className={styles.logoContainer}><Logo /></div>
-      <div className={styles.contactsContainer}><Contacts whiteColor={true}/></div>
-      <div className={styles.buttonContainer}>
-        <BurgerMenuButton handler={() => setIsNavMenuOpen(!isNavMenuOpen)}/>
+        <div className={styles.logoContainer}><Logo /></div>
+        <div className={styles.contactsContainer}><Contacts whiteColor={true}/></div>
+        <div className={styles.buttonContainer}>
+          <BurgerMenuButton handler={() => setIsNavMenuOpen(!isNavMenuOpen)}/>
+        </div>
+
+        {isShouldNavMount &&
+          <PopupLayout isNav={true} mountAnim={animStyle}>
+            <NavMenu closeNavHandler={() => setIsNavMenuOpen(false)} />
+          </PopupLayout>
+        }
+
       </div>
+    </>
 
-      {isShouldNavMount &&
-        <PopupLayout isNav={true} mountAnim={animStyle}>
-          <NavMenu closeNavHandler={() => setIsNavMenuOpen(false)} />
-        </PopupLayout>
-      }
-
-    </div>
   )
 }
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 import * as React from "react"
 import NotFound from "../components/sections/not-found/not-found";
-
+import Helmet from "react-helmet";
 const NotFoundPage = () => {
   return (
+    <>
+    <Helmet>
+      <title>Melatika - not found</title>
+    </Helmet>
     <main style={{ width: '100%', height: '100vh'}}>
       <NotFound />
     </main>
+    </>
+
   )
 }
 
